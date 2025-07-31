8176672dcc8239f71ab39456f73488357b7efff8
Zohar Peled <peled.zohar@gmail.com>
2025-07-31 09:56:27 +0300

    Add project files.

---
+++ src/Result.Simplified.Extensions/ResultOfTExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Result.Simplified.Extensions;
+
+public static class ResultOfTExtensions
+{
+    /// <summary>
+    /// Continues the operation only if the <see cref="Result{T}"/> is successful and the provided condition evaluates to <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Result{T}"/>.</typeparam>
+    /// <param name="self">The current <see cref="Result{T}"/>.</param>
+    /// <param name="condition">A predicate that should return <c>true</c> to continue the successful <see cref="Result{T}"/>.</param>
+    /// <param name="errorDescription">The error message in case the predicate evaluates to <c>false</c>.</param>
+    /// <returns>The original <see cref="Result{T}"/> if successful and condition is <c>true</c>; otherwise, a failed <see cref="Result{T}"/> with the provided error message.</returns>
+    public static Result<T> ThenIf<T>(
+        this Result<T> self,
+        Predicate<T> condition,
+        string errorDescription
+    ) => self.IsSuccess
+        ? condition(self.Value) ? self : Result<T>.Fail(errorDescription)
+        : self;
+
+    /// <summary>
+    /// Fails the <see cref="Result{T}"/> if it is successful and the provided fail condition evaluates to <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Result{T}"/>.</typeparam>
+    /// <param name="self">The current <see cref="Result{T}"/>.</param>
+    /// <param name="failCondition">A predicate that should return <c>true</c> to fail the <see cref="Result{T}"/>.</param>
+    /// <param name="errorDescription">The error message in case the predicate evaluates to <c>true</c>.</param>
+    /// <returns>The original <see cref="Result{T}"/> if successful and failCondition is <c>false</c>; otherwise, a failed <see cref="Result{T}"/> with the provided error message.</returns>
+    public static Result<T> ThenFailIf<T>(
+        this Result<T> self,
+        Predicate<T> failCondition,
+        string errorDescription
+    ) => self.IsSuccess
+        ? failCondition(self.Value) ? Result<T>.Fail(errorDescription) : self
+        : self;
+
+    /// <summary>
+    /// Continues the operation only if the <see cref="Result{T}"/> is a failure and the provided condition evaluates to <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Result{T}"/>.</typeparam>
+    /// <param name="self">The current <see cref="Result{T}"/>.</param>
+    /// <param name="condition">A predicate that should return <c>true</c> to continue the failed <see cref="Result{T}"/>.</param>
+    /// <param name="errorDescription">The error message in case the predicate evaluates to <c>false</c>.</param>
+    /// <returns>The original <see cref="Result{T}"/> if failed and condition is <c>true</c>; otherwise, a failed <see cref="Result{T}"/> with the provided error message.</returns>
+    public static Result<T> OtherwiseIf<T>(
+        this Result<T> self,
+        Predicate<T> condition,
+        string errorDescription
+    ) => !self.IsSuccess
+        ? condition(self.Value) ? self : Result<T>.Fail(errorDescription)
+        : self;
+
+    /// <summary>
+    /// Fails the <see cref="Result{T}"/> again if it is already a failure and the provided fail condition evaluates to <c>true</c>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Result{T}"/>.</typeparam>
+    /// <param name="self">The current <see cref="Result{T}"/>.</param>
+    /// <param name="failCondition">A predicate that should return <c>true</c> to apply an additional failure.</param>
+    /// <param name="errorDescription">The error message in case the predicate evaluates to <c>true</c>.</param>
+    /// <returns>The original <see cref="Result{T}"/> if failed and failCondition is <c>false</c>; otherwise, a new failed <see cref="Result{T}"/> with the provided error message.</returns>
+    public static Result<T> OtherwiseFailIf<T>(
+        this Result<T> self,
+        Predicate<T> failCondition,
+        string errorDescription
+    ) => !self.IsSuccess
+        ? failCondition(self.Value) ? Result<T>.Fail(errorDescription) : self
+        : self;
+}
---
+++ src/Result.Simplified.Extensions/VoidResultExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Result.Simplified.Extensions;
+
+public static class VoidResultExtensions
+{
+    /// <summary>
+    /// Continues the current <see cref="VoidResult"/> with another <see cref="VoidResult"/>, if the current <see cref="VoidResult"/> is successful.
+    /// </summary>
+    /// <param name="self">The current <see cref="VoidResult"/>.</param>
+    /// <param name="predicate">A predicate to build the next <see cref="VoidResult"/>. Success if evaluates to <c>true</c>, fail otherwise.</param>
+    /// <param name="errorDescription">The error description in case the <paramref name="predicate"/> evaluates to <c>false</c>.</param>
+    /// <returns>
+    /// <paramref name="self"/> if the current <see cref="VoidResult"/> has succeeded; 
+    /// otherwise, a new <see cref="VoidResult"/> instance based on <paramref name="predicate"/> and <paramref name="errorDescription"/>.
+    /// </returns>        
+    public static VoidResult ThenIf(
+        this VoidResult self,
+        Func<bool> predicate,
+        string errorDescription
+    ) => self.IsSuccess
+        ? VoidResult.SuccessIf(predicate, errorDescription)
+        : self;
+
+    /// <summary>
+    /// Continues the current <see cref="VoidResult"/> with another <see cref="VoidResult"/>, if the current <see cref="VoidResult"/> is successful.
+    /// </summary>
+    /// <param name="self">The current <see cref="VoidResult"/>.</param>
+    /// <param name="negativePredicate">A predicate to build the next <see cref="VoidResult"/>. Fail if evaluates to <c>true</c>, success otherwise.</param>
+    /// <param name="errorDescription">The error description in case the <paramref name="negativePredicate"/> evaluates to <c>true</c>.</param>
+    /// <returns>
+    /// <paramref name="self"/> if the current <see cref="VoidResult"/> has succeeded; 
+    /// otherwise, a new <see cref="VoidResult"/> instance based on <paramref name="negativePredicate"/> and <paramref name="errorDescription"/>.
+    /// </returns>
+    public static VoidResult ThenFailIf(
+        this VoidResult self,
+        Func<bool> negativePredicate,
+        string errorDescription
+    ) => self.IsSuccess
+        ? VoidResult.FailIf(negativePredicate, errorDescription)
+        : self;
+
+    /// <summary>
+    /// Continues the current <see cref="VoidResult"/> with another <see cref="VoidResult"/>, if the current <see cref="VoidResult"/> has failed.
+    /// </summary>
+    /// <param name="self">The current <see cref="VoidResult"/>.</param>
+    /// <param name="predicate">A predicate to build the next <see cref="VoidResult"/>. Success if evaluates to <c>true</c>, fail otherwise.</param>
+    /// <param name="errorDescription">The error description in case the <paramref name="predicate"/> evaluates to <c>false</c>.</param>
+    /// <returns>
+    /// <paramref name="self"/> if the current <see cref="VoidResult"/> has failed; 
+    /// otherwise, a new <see cref="VoidResult"/> instance based on <paramref name="predicate"/> and <paramref name="errorDescription"/>.
+    /// </returns>
+    public static VoidResult OtherwiseIf(
+        this VoidResult self,
+        Func<bool> predicate,
+        string errorDescription
+    ) => !self.IsSuccess
+        ? VoidResult.SuccessIf(predicate, errorDescription)
+        : self;
+
+    /// <summary>
+    /// Continues the current <see cref="VoidResult"/> with another <see cref="VoidResult"/>, if the current <see cref="VoidResult"/> has failed.
+    /// </summary>
+    /// <param name="self">The current <see cref="VoidResult"/>.</param>
+    /// <param name="negativePredicate">A predicate to build the next <see cref="VoidResult"/>. Fail if evaluates to <c>true</c>, success otherwise.</param>
+    /// <param name="errorDescription">The error description in case the <paramref name="negativePredicate"/> evaluates to <c>true</c>.</param>
+    /// <returns>
+    /// <paramref name="self"/> if the current <see cref="VoidResult"/> has failed; 
+    /// otherwise, a new <see cref="VoidResult"/> instance based on <paramref name="negativePredicate"/> and <paramref name="errorDescription"/>.
+    /// </returns>
+    public static VoidResult OtherwiseFailIf(
+        this VoidResult self,
+        Func<bool> negativePredicate,
+        string errorDescription
+    ) => !self.IsSuccess
+        ? VoidResult.FailIf(negativePredicate, errorDescription)
+        : self;
+}
